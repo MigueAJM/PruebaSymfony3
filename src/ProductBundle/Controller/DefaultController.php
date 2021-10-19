24f156495dfaa63fb895edc29c36c66505014fd4
Miguel Angel Jimenez <miguejm@outlook.es>
2021-10-18 20:52:32 -0500

    Uso de plantillas Twig

--- src/ProductBundle/Controller/DefaultController.php
+++ src/ProductBundle/Controller/DefaultController.php
@@ -8,7 +8,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/" name="product_home")
+     * @Route("/")
      */
     public function indexAction()
     {
